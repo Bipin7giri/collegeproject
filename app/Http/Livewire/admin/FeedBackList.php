b5d006f8140b75c326c879e68b132573558329d1
Bipin Giri <bipingiri27@gmail.com>
2022-07-27 23:04:23 +0545

    final

--- app/Http/Livewire/admin/FeedBackList.php
+++ app/Http/Livewire/admin/FeedBackList.php
@@ -3,13 +3,19 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\FeedBack;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class FeedBackList extends Component
 {
+    public $rating;
     public function render()
     {
+        $count  = DB::table('feed_back')->where('comment','like','%'.'good'.'%')->orWhere('comment','like','%'.'ok'.'%')->orWhere('comment','like','%'.'best'.'%')->orWhere('comment','like','%'.'superb'.'%')->orWhere('comment','like','%'.'outsanding'.'%')->count();
+        $total  = DB::table('feed_back')->count();
+       
+        $this->rating = ($count/$total)*100;
         $feedBacks= FeedBack::all();
-        return view('livewire.admin.feed-back-list',["feedBacks"=>$feedBacks])->layout('layouts.dashboard');
+        return view('livewire.admin.feed-back-list',["feedBacks"=>$feedBacks,"count"=>$count])->layout('layouts.dashboard');
     }
 }
